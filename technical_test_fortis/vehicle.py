5fa1691365ea4d033dafa572f290727e687d3a09
Kevin <kevin.moesker@trimoteq.com>
2024-07-22 00:04:33 +0200

    Add documentation to Vehicle class
    This commit introduces detailed docstring for the Vehicle class. It includes a general description, attributes, and methods of the class. This provides a clearer understanding of the vehicle's characteristics and functionality.

--- technical_test_fortis/vehicle.py
+++ technical_test_fortis/vehicle.py
@@ -2,6 +2,24 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):
+    """
+
+    The Vehicle class is an abstract base class that represents a generic vehicle.
+
+    Attributes:
+        - year (int): The year of manufacture of the vehicle.
+        - tank_size (int): The size of the vehicle's fuel tank.
+        - consumption (int): The fuel consumption rate of the vehicle in liters per kilometer.
+
+    Methods:
+        - __init__(year: int, tank_size: int, consumption: int) -> None:
+            Initializes a new instance of the Vehicle class.
+            Raises a ValueError if any of the input arguments are invalid.
+
+        - compute_maximal_distance() -> int:
+            Computes and returns the maximal distance that the vehicle can travel with its current fuel.
+
+    """
     def __init__(self, year: int, tank_size: int, consumption: int) -> None:
 
         if not isinstance(year, int) or year <= 0:
