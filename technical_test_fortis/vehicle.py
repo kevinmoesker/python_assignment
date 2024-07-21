ee7cfed0da6084a489928a77227e966a1a4656ef
Kevin <kevin.moesker@trimoteq.com>
2024-07-21 21:08:30 +0200

    Add Bike subclass to Vehicle class
    A new subclass named Bike was added to the Vehicle class. This Bike class includes attributes specific to bikes, such as saddle comfort, and overrides the 'compute_maximal_distance' method from its parent class. The implementation of saddle_comfort doubles the base distance if the saddle is comfortable.

--- technical_test_fortis/vehicle.py
+++ technical_test_fortis/vehicle.py
@@ -51,10 +51,44 @@ class Car(Vehicle):
             theoretical_max_distance *= 1.1
 
         return int(theoretical_max_distance)
-"""
-Gabriel: introduce class Bike
-"""
 
+
+class Bike(Vehicle):
+    """
+    This class represents a bike. It is a subclass of Vehicle.
+
+    Attributes:
+    - year (int): The year of the bike's manufacture.
+    - tank_size (int): This attribute does not apply to bikes. Set to 0 as default.
+    - consumption (int): This attribute represents the consumption of energy of the rider per km, typically it has a very low value.
+    - saddle_comfort (bool): Whether the bike has a very comfortable saddle or not.
+
+    Methods:
+    - __init__(year: int, consumption: int, saddle_comfort: bool) -> None:
+       Initializes the Bike object with the given year, consumption, and saddle_comfort status.
+
+    - compute_maximal_distance() -> int:
+        Computation of how far a person can travel on this Bike under ideal circumstances.
+        The impact of saddle_comfort is to double the distance if it is comfortable.
+    """
+
+    def __init__(self, year: int, consumption: int, saddle_comfort: bool) -> None:
+        super().__init__(year, 0, consumption)
+        self.saddle_comfort = saddle_comfort
+
+    def compute_maximal_distance(self) -> int:
+        """
+        This method overrides the abstract method in the parent class.
+        It returns twice the distance if the saddle is comfortable, in contrast to the Car class where the mileage
+        is affected by the technical inspection and the year of manufacture.
+        """
+        base_distance = 100 / self.consumption
+
+        if self.saddle_comfort:
+            # If saddle is comfortable, the base distance is doubled.
+            return int(2 * base_distance)
+        else:
+            return int(base_distance)
 """
 Task 2:
 Create a find_best_vehicle function that takes 2 vehicles as argument and return the one with the highest autonomy.
